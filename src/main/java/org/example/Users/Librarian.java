c0695907c981abf3203419041bb1c3aafd7f6943
david <david@plisly.net>
2025-02-22 10:56:35 +0000

    adding users

---
+++ src/main/java/org/example/Users/Librarian.java
@@ -0,0 +1,7 @@
+package org.example.Users;
+
+public class Librarian implements User {
+    public void printUser() {
+        System.out.println("I am a librarian");
+    }
+}
---
+++ src/main/java/org/example/Users/User.java
@@ -0,0 +1,5 @@
+package org.example.Users;
+
+public interface User {
+    public void printUser();
+}
